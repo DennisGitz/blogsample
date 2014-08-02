82f5e443cbcc2bb4861c35bb3a40ccfd0b19ce89
DennnisGitz <dennisgitonga90@gmail.com>
2014-08-02 16:03:56 +0300

    New Files

---
+++ app.js
@@ -0,0 +1,67 @@
+/*jshint node:true*/
+
+// app.js
+// This file contains the server side JavaScript code for your application.
+// This sample application uses express as web application framework (http://expressjs.com/),
+// and jade as template engine (http://jade-lang.com/).
+
+
+// setup middleware
+var express = require('express')
+  , app = express() // Web framework to handle routing requests
+  , cons = require('consolidate') // Templating library adapter for Express
+  , MongoClient = require('mongodb').MongoClient // Driver for connecting to MongoDB
+  , routes = require('./routes'); 
+
+  // There are many useful environment variables available in process.env.
+// VCAP_APPLICATION contains useful information about a deployed application.
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+// TODO: Get application information and use it in your app.
+
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+// TODO: Get service credentials and communicate with bluemix services.
+if (process.env.VCAP_SERVICES) {
+   var env = JSON.parse(process.env.VCAP_SERVICES);
+   var mongo = env['mongodb-2.2'][0].credentials;
+} else {
+   var mongo = {
+      "username" : "user1",
+      "password" : "secret",
+      "url" : "mongodb://user1:secret@localhost:27017/myblog"
+};
+// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+// The port on the DEA for communication with the application:
+var port = (process.env.VCAP_APP_PORT || 3000);
+// Start server
+
+
+// render index page
+MongoClient.connect(mongo.url, function(err, db) {
+    
+    if(err) throw err;
+
+    // Register our templating engine
+    app.engine('html', cons.swig);
+    app.set('view engine', 'html');
+    app.set('views', __dirname + '/views');
+
+    // Express middleware to populate 'req.cookies' so we can access cookies
+    app.use(express.cookieParser());
+
+    // Express middleware to populate 'req.body' so we can access POST variables
+    app.use(express.bodyParser());
+
+    // Application routes
+    routes(app, db);
+
+   app.listen(port, host);
+console.log('App started on port ' + port);
+    //console.log('Express server listening on port 3000');
+});
+
+
+
